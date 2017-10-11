feea724a71f7ea7845298c6cd267813aa7f085f3
mim Armand <mim3dot@gmail.com>
2017-10-10 23:18:07 -0500

    Makng greetings a little bit funnier! :D

--- msg_greetings.js
+++ msg_greetings.js
@@ -3,11 +3,14 @@
 /* eslint quote-props: ['error', 'consistent']*/
 module.exports = {
     messages: [
-        "Hi, how can I help you today?",
-        "Hello! let me know what you'd like to know in crypto-market?",
         "Hey buddy! which coin you want to know the price of?",
         "Hello sunshine! pick a crypto coin so I get its status for ya sweety!",
-        "Welcome! Which coin you are interested in?",
-        "Yo! What coin?!"
+        "Howdy, partner! Give me the name of a coin and I'll give you its price!",
+        "Hey, howdy, hi! say the coin name to find if it's low or high!",
+        "What’s kickin’, little chicken? say the coin name to get begin!",
+        "Hiya! give me a coin name so I give its price to ya!",
+        "Yo! yo! which coin you want to know the price for?!",
+        "Ello, gov'nor! which coin you are interested to get the price for?",
+        "Sup, homeslice? say a coin name and get the price"
     ]
 }
